2d68405bd60f2a75e45b0341d0cb87cebe4b23e6
Muhammad Radya Iftikhar <radyaiftikhar@gmail.com>
2023-04-15 12:07:44 +0700

    Add Hak Akses

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -21,7 +21,11 @@ class AuthController extends Controller
         } catch (JWTException $e) {
             return response()->json(['error' => 'could_not_create_token'], 500);
         }
-        return response()->json(compact('token'));
+
+        $user = JWTAuth::User();
+        $level = $user->level;
+
+        return response()->json(compact('token','level'));
     }
 
     public function register(Request $request)
