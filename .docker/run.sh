26edc633a4208654033ed3232955c1844f9d382a
jesusfb <jfigueroa.beltran@gmail.com>
2023-09-09 13:58:30 -0700

    Update run.sh

--- .docker/run.sh
+++ .docker/run.sh
@@ -7,8 +7,9 @@ mkdir -p /run/nginx
 
 cd /var/www
 
-# php artisan migrate:fresh --seed
+php artisan migrate:fresh --seed
 php artisan cache:clear
 php artisan route:cache
+php artisan key:generate
 
 /usr/bin/supervisord -c /etc/supervisord.conf
